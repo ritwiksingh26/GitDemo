cfc3e64c3c70b29e982719f7f05dbe426ac9fa65
Ritwik Singh <ritwiksingh26@gmail.com>
2025-05-22 13:54:23 +0530

    new branch file from gitStuff

---
+++ src/GitStuffBranchNewFile.java
@@ -0,0 +1,9 @@
+public class GitStuffBranchNewFile {
+    public static void main(String[] args) {
+        System.out.println("This is new branch file");
+        System.out.println("This is new branch file 2");
+        System.out.println("This is new branch file 3");
+        System.out.println("This is new branch file 4");
+        System.out.println("This is new branch file 5");
+    }
+}
