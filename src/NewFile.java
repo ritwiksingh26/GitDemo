dc771be03bcff0eb56610cbab95deb2df4b67391
Ritwik Singh <ritwiksingh26@gmail.com>
2025-05-22 12:41:39 +0530

    new commit after pull

--- src/NewFile.java
+++ src/NewFile.java
@@ -1,5 +1,7 @@
 public class NewFile {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("new commit");
+        System.out.println("new commit 2");
     }
 }
